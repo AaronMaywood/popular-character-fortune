51a90a8f675f5c6abac12803314358d8423fac65
Aaron Maywood <am7274183@gmail.com>
2023-12-27 14:03:18 +0900

    誕生数1〜9のテストを実装した

--- src/util.test.js
+++ src/util.test.js
@@ -1,6 +1,38 @@
 import { calcBirthNumber } from './util.js';
 
-test('2000-1-1', () => {
+test('2000-1-1 誕生数4', () => {
 	expect(calcBirthNumber(2000,1,1)).toBe(4);
+})
+
+test('2000-1-2 誕生数５', () => {
+	expect(calcBirthNumber(2000,1,2)).toBe(5);
+})
+
+test('2000-1-3 誕生数６', () => {
+	expect(calcBirthNumber(2000,1,3)).toBe(6);
+})
+
+test('2000-1-4 誕生数７', () => {
+	expect(calcBirthNumber(2000,1,4)).toBe(7);
+})
+
+test('2000-1-5 誕生数８', () => {
+	expect(calcBirthNumber(2000,1,5)).toBe(8);
+})
+
+test('2000-1-6 誕生数９', () => {
+	expect(calcBirthNumber(2000,1,6)).toBe(9);
+})
+
+test('2000-1-7 誕生数１', () => {
+	expect(calcBirthNumber(2000,1,7)).toBe(1);
+})
+
+test('2000-1-8 誕生数２', () => {
+	expect(calcBirthNumber(2000,1,8)).toBe(2);
+})
+
+test('2000-1-9 誕生数３', () => {
+	expect(calcBirthNumber(2000,1,9)).toBe(3);
 })
 
