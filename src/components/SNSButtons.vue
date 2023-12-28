6a0966baf08b965b5100f5189d82ea44d64bf80c
Aaron Maywood <am7274183@gmail.com>
2023-12-28 15:24:17 +0900

    bugfix
    - https://i.gyazo.com/a693a7cfb6637ffa4dd10cfdc94c89c5.png の解消
    
    Hint: https://stackoverflow.com/questions/68803137/vue-3-passing-array-warning-extraneous-non-props-attributes-were-passed-to-comp
        →ルート要素が一つになるようにせよ

--- src/components/SNSButtons.vue
+++ src/components/SNSButtons.vue
@@ -106,40 +106,42 @@ onUpdated(() => {
 </script>
 
 <template>
-	<!--
-	<div id="template" :class="props.birthNumber"></div>
-	-->
-	<div id="fb-root"></div>
-	<div id="snsbtnArea" class="clearfix">
-		<div id="snsbtnArea-in">
-			<!-- LINE Send Button -->
-			<div
-				class="line-it-button"
-				:data-url="data_url"
-				data-lang="ja"
-				data-type="share-a"
-				data-env="REAL"
-				data-color="default"
-				data-size="small"
-				data-count="false"
-				data-ver="3"
-				style="display: none"
-			></div>
-			<!-- X Tweet Button -->
-			<a
-				href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-				data-text="流行りキャラ占い"
-				:data-url="data_url"
-				class="twitter-share-button"
-				data-show-count="false"
-				>Tweet</a
-			>
-			<!-- Facebook Share Button -->
-			<div
-				class="fb-share-button"
-				:data-href="data_url"
-				data-layout="button"
-			></div>
+	<div id="wrapper">
+		<!--
+		<div id="template" :class="props.birthNumber"></div>
+		-->
+		<div id="fb-root"></div>
+		<div id="snsbtnArea" class="clearfix">
+			<div id="snsbtnArea-in">
+				<!-- LINE Send Button -->
+				<div
+					class="line-it-button"
+					:data-url="data_url"
+					data-lang="ja"
+					data-type="share-a"
+					data-env="REAL"
+					data-color="default"
+					data-size="small"
+					data-count="false"
+					data-ver="3"
+					style="display: none"
+				></div>
+				<!-- X Tweet Button -->
+				<a
+					href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+					data-text="流行りキャラ占い"
+					:data-url="data_url"
+					class="twitter-share-button"
+					data-show-count="false"
+					>Tweet</a
+				>
+				<!-- Facebook Share Button -->
+				<div
+					class="fb-share-button"
+					:data-href="data_url"
+					data-layout="button"
+				></div>
+			</div>
 		</div>
 	</div>
 </template>
